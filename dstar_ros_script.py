be2add24d39468c956cc50722c233d0795d353ab
Ashvin Iyer <ashvin.iyer@gmail.com>
2023-02-18 10:34:50 -0500

    updated dstar ros

--- dstar_ros_script.py
+++ dstar_ros_script.py
@@ -4,33 +4,42 @@ from std_msgs.msg import Float32MultiArray
 from nav_msgs.msg import Odometry, OccupancyGrid
 from tf.transformations import euler_from_quaternion
 import dstar_ros
-import numpy
+import numpy as np
 
 def main():
-
-    goal = [0, 0]
-    start = [0, 0]
-    init_map = numpy.array([])
 
     radius = 8 #robot rad (grid units)
 
     frequency = 10 #hz
 
-    dstar = dstar_ros.Dstar(goal, start, init_map, radius)
+    pose = np.array([])
+
+    map = np.array([])
+
+    update_map = False
+
+    goal = np.array([])
+
+    dstar = None
 
     path_publisher = rospy.Publisher("/path", Float32MultiArray, queue_size = 10)
 
     def grid_subscriber(data):
-        dstar.update_map(data)
+        global update_map, map
+        width = data.info.width
+        height = data.info.height
+        map = np.reshape(data.data (height, width))
+        update_map = True
 
     rospy.Subscriber("/map", OccupancyGrid, grid_subscriber)
 
     def position_subscriber(data):
+        global pose
         position = data.pose.pose.position
 
         coords = [position.x, position.y]
 
-        dstar.update_position(coords)
+        pose = coords
 
     rospy.Subscriber("/odom", Odometry, position_subscriber)
 
@@ -40,7 +49,7 @@ def main():
 
     path = []
     while not rospy.is_shutdown():
-        if (not completedInitialRun and len(init_map) > 0):
+        if (not completedInitialRun and len(dstar.currentMap) > 0):
             dstar.find_path(True)
             completedInitialRun = True
 
